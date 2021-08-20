911b6bf702b2757cf08214245701330bb04b3845
imrnk <sujoy.imran@gmail.com>
2021-08-20 21:20:54 +0530

    IO Error handling exercise

--- src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
@@ -8,7 +8,8 @@ object IOErrorHandling {
 
   // IO: pure, delay, defer
   // create failed effects
-  val aFailedCompute: IO[Int] = IO.delay(throw new RuntimeException("A FAILURE"))
+  val aFailedCompute: IO[Int] =
+    IO.delay(throw new RuntimeException("A FAILURE"))
   val aFailure: IO[Int] = IO.raiseError(new RuntimeException("a proper fail"))
 
   // handle exceptions
@@ -20,30 +21,34 @@ object IOErrorHandling {
   // turn into an Either
   val effectAsEither: IO[Either[Throwable, Int]] = aFailure.attempt
   // redeem: transform the failure and the success in one go
-  val resultAsString: IO[String] = aFailure.redeem(ex => s"FAIL: $ex", value => s"SUCCESS: $value")
+  val resultAsString: IO[String] =
+    aFailure.redeem(ex => s"FAIL: $ex", value => s"SUCCESS: $value")
   // redeemWith
-  val resultAsEffect: IO[Unit] = aFailure.redeemWith(ex => IO(println(s"FAIL: $ex")), value => IO(println(s"SUCCESS: $value")))
+  val resultAsEffect: IO[Unit] = aFailure.redeemWith(
+    ex => IO(println(s"FAIL: $ex")),
+    value => IO(println(s"SUCCESS: $value"))
+  )
 
   /**
-   * Exercises
-   */
+    * Exercises
+    */
   // 1 - construct potentially failed IOs from standard data types (Option, Try, Either)
   def option2IO[A](option: Option[A])(ifEmpty: Throwable): IO[A] =
     option match {
-      case Some(value) => IO.pure(value)
-      case None => IO.raiseError(ifEmpty)
+      case Some(value) => IO(value)
+      case None        => IO.raiseError(ifEmpty)
     }
 
   def try2IO[A](aTry: Try[A]): IO[A] =
     aTry match {
-      case Success(value) => IO.pure(value)
-      case Failure(ex) => IO.raiseError(ex)
+      case Success(value)     => IO(value)
+      case Failure(exception) => IO.raiseError(exception)
     }
 
   def either2IO[A](anEither: Either[Throwable, A]): IO[A] =
     anEither match {
-      case Left(ex) => IO.raiseError(ex)
-      case Right(value) => IO.pure(value)
+      case Left(error)  => IO.raiseError(error)
+      case Right(value) => IO(value)
     }
 
   // 2 - handleError, handleErrorWith
